a3781a13c033a0565ddcccd709405d64d461fd62
Kobe McKee <kobe.mckee@gmail.com>
2019-12-11 15:56:19 -0700

    added MenuItem model

---
+++ R&R-BBQ/MenuItem.swift
@@ -0,0 +1,22 @@
+//
+//  MenuItem.swift
+//  R&R-BBQ
+//
+//  Created by Kobe McKee on 12/11/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct MenuItem {
+    
+    var name: String
+    var image: UIImage?
+    var price: Double
+    
+    //var sides: Side
+    var info: String?
+    var options: [String]?
+    
+}
